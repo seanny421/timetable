9465eb668a047616b5d1d0905cf300fa6c1ed6b0
seanny421 <sean5599@hotmail.co.uk>
2021-03-02 22:57:24 +0000

    added ability to push a todo a day forward

---
+++ src/Pomodoro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+
+function Pomodoro(){
+
+    return(
+        <h1>HELLO</h1>
+    );
+}
+
+
+export default Pomodoro;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function Todo({todos, setTodos, todoText, todo, today}){
     const [isDeleted, setIsDeleted] = useState(false);
+    const [isNextDay, setIsNextDay] = useState(false);
 
     function deleteHandler(e){
         setIsDeleted(true);
@@ -19,11 +20,50 @@ function Todo({todos, setTodos, todoText, todo, today}){
             return item;
         }))
     };
+
+    function getDay(todoDay){
+        switch(todoDay){
+          case "Monday":
+              return "Sunday"
+          case "Sunday":
+              return "Monday"
+          case "Monday":
+              return "Tuesday"
+          case "Tuesday":
+              return "Wednesday"
+          case "Wednesday":
+              return "Thursday"
+          case "Thursday":
+              return "Friday"
+          case "Friday":
+              return "Saturday"
+        }
+        
+      }
+
+    function nextDayHandler(){
+        setIsNextDay(true)
+        //set this todo.day to the next day
+        setTimeout(() => {
+            setTodos(todos.map((item) => {
+                if(item.id === todo.id){
+                    return{
+                        ...item, day: getDay(item.day)
+                    }
+                }
+                return item;
+            }))
+        }, 500);
+    }
+
     return(
-        <div className={`todo ${isDeleted ? "fall": ""} ${todo.completed ? "completed": ""}`}>
+        <div className={`todo ${isDeleted ? "fall": ""} ${todo.completed ? "completed": ""} ${isNextDay ? "slide": ""}`}>
             <li className={`todo-item`}>{todoText}</li>
             <button onClick={completedHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
+            </button>
+            <button onClick={nextDayHandler} className={'move-day-btn'}>
+                <i className="fas fa-arrow-right"></i>
             </button>
             <button onClick={deleteHandler} className="trash-btn">
                 <i className="fas fa-trash"></i>
@@ -31,6 +71,7 @@ function Todo({todos, setTodos, todoText, todo, today}){
         </div>
     );
 };
+
 
 
 export default Todo;
